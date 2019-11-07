2058dac974a5b7755a89cdfb25e3e7ed8b1efdb8
Ivilin Stoianov <ivilin.stoianov@gmail.com>
2019-11-07 23:45:12 +0100

    Update mlp_init.m

--- mlp_init.m
+++ mlp_init.m
@@ -1,11 +1,12 @@
 % Creates a new 2-layer multilayer perceptron.
 % Network size is given in the input parameter sz
 % sz=[n-input units, n-hidden units, n-output units]
-% N  a structure with the layer weights.
+% Returns variable N: a structure containing the layer weights, ready to be trained
 
 function N=mlp_init(sz)
 
-N.sz=sz;                  		% network size (we expect 3 layers)
+N.sz=sz;                  		  % network size (we expect 3 layers)
+N.nlayers = numel(sz);        	% the total number of the layers (including inp and out)
 
 N.hw=randn(sz(1),sz(2));        % random weights of the hidden layer (n-inp x n-hid)
 N.hb=zeros(1,sz(2));           	% null bias
